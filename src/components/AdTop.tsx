b2d1fc8bbe1b8752fd1f1008f46627ad597469bf
Lalit Kumar <37037458+lalitkalyan@users.noreply.github.com>
2025-08-16 04:43:17 -0700

    feat: layout + ad placeholders

---
+++ src/components/AdTop.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdTop: React.FC = () => {
+  return (
+    <div id="ads-top" className="w-full flex justify-center items-center my-4">
+      {/* Placeholder for top ad */}
+    </div>
+  );
+};
+
+export default AdTop;
