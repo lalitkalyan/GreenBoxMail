306f49e51092627766d16868e643f1f717f7e480
Lalit Kumar <37037458+lalitkalyan@users.noreply.github.com>
2025-08-16 04:45:09 -0700

    feat: layout + ad placeholders

---
+++ src/components/AdBottom.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdBottom: React.FC = () => {
+  return (
+    <div id="ads-bottom" className="w-full flex justify-center items-center my-4">
+      {/* Placeholder for bottom ad */}
+    </div>
+  );
+};
+
+export default AdBottom;
