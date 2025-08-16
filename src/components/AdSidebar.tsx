e32bb70f7547669fe29914b14f6bcb926eb586d5
Lalit Kumar <37037458+lalitkalyan@users.noreply.github.com>
2025-08-16 04:43:54 -0700

    feat: layout + ad placeholders

---
+++ src/components/AdSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdSidebar: React.FC = () => {
+  return (
+    <aside id="ads-sidebar" className="hidden lg:block lg:w-1/4 p-2">
+      {/* Placeholder for sidebar ad */}
+    </aside>
+  );
+};
+
+export default AdSidebar;
