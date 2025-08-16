fd6842caa86c0a4ed7f245c6700b6ead3839af8a
Lalit Kumar <37037458+lalitkalyan@users.noreply.github.com>
2025-08-16 06:18:04 -0700

    feat: ads config placeholders + docs

---
+++ src/config/ads.ts
@@ -0,0 +1,7 @@
+/**
+ * Ad placeholder identifiers.
+ * You can configure your ad network IDs here.
+ */
+export const ADS_TOP_ID = 'ads-top';
+export const ADS_SIDEBAR_ID = 'ads-sidebar';
+export const ADS_BOTTOM_ID = 'ads-bottom';
